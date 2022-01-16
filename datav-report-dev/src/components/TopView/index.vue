fbf3361f59ea23030e466e4a1000b921d5fcc7bc
AurorePaladin <aurorepaladin@gmail.com>
2022-01-16 22:23:13 +0800

    创建TopView

---
+++ datav-report-dev/src/components/TopView/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="top-view">
+    这是TopView
+      <el-col :span="6">
+        <el-card shadow="hover">
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+        </el-card>
+      </el-col>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
