929f2a4abb5bc435c368d876412e5acf50e69f31
AurorePaladin <aurorepaladin@gmail.com>
2022-01-18 21:52:53 +0800

    累计订单量量header和footer开发

--- datav-report-dev/src/components/TotalOrders/index.vue
+++ datav-report-dev/src/components/TotalOrders/index.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>累计订单
-    <common-card></common-card>
-    </div>
+    <common-card title="累积订单量" value="￥ 2,157,420">
+    <template v-slot:footer>
+        <span>昨日订单量</span>
+        <span class="emphasis">￥ 200,000,000</span>
+    </template>
+    </common-card>
 </template>
 
 <script>
