10862e91651aac0457a3e63afb8e6e47a3d92b9c
AurorePaladin <aurorepaladin@gmail.com>
2022-01-18 22:20:20 +0800

    今日交易用户-head和footer完成

--- datav-report-dev/src/components/TodayUsers/index.vue
+++ datav-report-dev/src/components/TodayUsers/index.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>今日用户
-        <common-card></common-card>
-    </div>
+    <common-card title="今日交易用户" value="81,014">
+    <template v-slot:wrapper>
+        <div id="total-orders-chart" :style="{ width: '100%',height: '100%'}"></div>
+    </template>
+    <template v-slot:footer>
+        <span>退货率</span>
+        <span class="emphasis">5.14%</span>
+    </template>
+    </common-card>
 </template>
 
 <script>
