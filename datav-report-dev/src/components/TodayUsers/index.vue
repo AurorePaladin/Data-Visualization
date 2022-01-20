d290a02d97bd86266ec0608ef5a626e7e4b25e40
AurorePaladin <aurorepaladin@gmail.com>
2022-01-20 22:05:33 +0800

    用vue-echarts改造累计用户

--- datav-report-dev/src/components/TodayUsers/index.vue
+++ datav-report-dev/src/components/TodayUsers/index.vue
@@ -1,7 +1,8 @@
 <template>
     <common-card title="今日交易用户" value="81,014">
     <template v-slot:wrapper>
-        <div id="today-users-chart" :style="{ width: '100%',height: '100%'}"></div>
+        <!-- <div id="today-users-chart" :style="{ width: '100%',height: '100%'}"></div> -->
+        <v-chart :option="getOptions()" />
     </template>
     <template v-slot:footer>
         <span>退货率</span>
@@ -14,32 +15,58 @@
 import commonCardMixin from '../../mixins/commonCardMixin'
 export default {
     mixins: [commonCardMixin],
-    mounted() {
-        const chartDom = document.getElementById('today-users-chart')
-        const chart = this.$echarts.init(chartDom)
-        chart.setOption({
-            color: ['#3398DB'],
-            xAxis: {
-                type: 'category',
-                data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00'],
-                show: false,
-                barWidth: '60%'
-            },
-            yAxis: {
-                show: false,
-            },
-            series: [{
-                type: 'bar',
-                data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 290, 134, 290, 330, 150],
-            }],
-            grid: {
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
+    methods: {
+        getOptions: function () {
+            return {
+                color: ['#3398DB'],
+                xAxis: {
+                    type: 'category',
+                    data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00'],
+                    show: false,
+                    barWidth: '60%'
+                },
+                yAxis: {
+                    show: false,
+                },
+                series: [{
+                    type: 'bar',
+                    data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 290, 134, 290, 330, 150],
+                }],
+                grid: {
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                }
             }
-        })
+        }
     },
+    // mounted() {
+    //     const chartDom = document.getElementById('today-users-chart')
+    //     const chart = this.$echarts.init(chartDom)
+    //     chart.setOption({
+    //         color: ['#3398DB'],
+    //         xAxis: {
+    //             type: 'category',
+    //             data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00'],
+    //             show: false,
+    //             barWidth: '60%'
+    //         },
+    //         yAxis: {
+    //             show: false,
+    //         },
+    //         series: [{
+    //             type: 'bar',
+    //             data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 290, 134, 290, 330, 150],
+    //         }],
+    //         grid: {
+    //             top: 0,
+    //             bottom: 0,
+    //             left: 0,
+    //             right: 0,
+    //         }
+    //     })
+    // },
 }
 </script>
 
--- datav-report-dev/src/components/TotalUsers/index.vue
+++ datav-report-dev/src/components/TotalUsers/index.vue
@@ -2,7 +2,7 @@
     <common-card title="累计用户数" value="1,087,503">
     <template v-slot:wrapper>
         <!-- <div id="total-users-chart" :style="{ width: '100%',height: '100%'}"></div> -->
-        <v-charts :option="getOptions()" />
+        <v-chart :option="getOptions()" />
     </template>
     <template v-slot:footer>
         <div class="total-users-footer">
@@ -27,79 +27,79 @@ export default {
                 xAxis: {
                 type: 'value',
                 show: false,
-            },
-            yAxis: {
-                type: 'category',
-                show: false,
-            },
-            series: [{
-                type: 'bar',
-                stack: '总量',
-                data: [200],
-                barWidth: 10,
-                itemStyle: {
-                    color: '#45c946'
                 },
-            },
-            {
-                type: 'bar',
-                stack: '总量',
-                data: [250],
-                barWidth: 10,
-                itemStyle: {
-                    color: '#eee'
+                yAxis: {
+                    type: 'category',
+                    show: false,
                 },
-            },
-            {
-                type: 'custom',
-                stack: '总量',
-                data: [200],
-                renderItem: (params, api) => {
-                    // console.log(params, api);
-                    const value = api.value(0)
-                    const endPoint = api.coord([value, 0])
-                    console.log(endPoint)
-                    return {
-                        type: 'group',
-                        positions: endPoint,
-                        children: [{
-                            type: 'path',
-                            position: endPoint,
-                            shape: {
-                                d: 'M886.6 694.6c-6.2 6.2-14.4 9.4-22.6 9.4H160c-8.2 0-16.4-3.1-22.6-9.4-6.2-6.2-9.4-14.4-9.4-22.6s3.1-16.4 9.4-22.6l352-352c6.2-6.2 14.4-9.4 22.6-9.4s16.4 3.1 22.6 9.4l352 352c6.2 6.2 9.4 14.4 9.4 22.6s-3.1 16.4-9.4 22.6z',
-                                x: -5,
-                                y: 7,
-                                width: 10,
-                                height: 10,
+                series: [{
+                    type: 'bar',
+                    stack: '总量',
+                    data: [200],
+                    barWidth: 10,
+                    itemStyle: {
+                        color: '#45c946'
+                    },
+                },
+                {
+                    type: 'bar',
+                    stack: '总量',
+                    data: [250],
+                    barWidth: 10,
+                    itemStyle: {
+                        color: '#eee'
+                    },
+                },
+                {
+                    type: 'custom',
+                    stack: '总量',
+                    data: [200],
+                    renderItem: (params, api) => {
+                        // console.log(params, api);
+                        const value = api.value(0)
+                        const endPoint = api.coord([value, 0])
+                        console.log(endPoint)
+                        return {
+                            type: 'group',
+                            positions: endPoint,
+                            children: [{
+                                type: 'path',
+                                position: endPoint,
+                                shape: {
+                                    d: 'M886.6 694.6c-6.2 6.2-14.4 9.4-22.6 9.4H160c-8.2 0-16.4-3.1-22.6-9.4-6.2-6.2-9.4-14.4-9.4-22.6s3.1-16.4 9.4-22.6l352-352c6.2-6.2 14.4-9.4 22.6-9.4s16.4 3.1 22.6 9.4l352 352c6.2 6.2 9.4 14.4 9.4 22.6s-3.1 16.4-9.4 22.6z',
+                                    x: -5,
+                                    y: 7,
+                                    width: 10,
+                                    height: 10,
+                                },
+                                style: {
+                                    fill: '#45c946'
+                                }
                             },
-                            style: {
-                                fill: '#45c946'
-                            }
-                        },
-                        {
-                            type: 'path',
-                            position: endPoint,
-                            shape: {
-                                d: 'M886.6 329.4c-6.2-6.2-14.4-9.4-22.6-9.4H160c-8.2 0-16.4 3.1-22.6 9.4-6.2 6.2-9.4 14.4-9.4 22.6s3.1 16.4 9.4 22.6l352 352c6.2 6.2 14.4 9.4 22.6 9.4s16.4-3.1 22.6-9.4l352-352c6.2-6.2 9.4-14.4 9.4-22.6s-3.1-16.4-9.4-22.6z',
-                                x: -5,
-                                y: -17,
-                                width: 10,
-                                height: 10,
-                            },
-                            style: {
-                                fill: '#45c946'
-                            }
-                        }],
+                            {
+                                type: 'path',
+                                position: endPoint,
+                                shape: {
+                                    d: 'M886.6 329.4c-6.2-6.2-14.4-9.4-22.6-9.4H160c-8.2 0-16.4 3.1-22.6 9.4-6.2 6.2-9.4 14.4-9.4 22.6s3.1 16.4 9.4 22.6l352 352c6.2 6.2 14.4 9.4 22.6 9.4s16.4-3.1 22.6-9.4l352-352c6.2-6.2 9.4-14.4 9.4-22.6s-3.1-16.4-9.4-22.6z',
+                                    x: -5,
+                                    y: -17,
+                                    width: 10,
+                                    height: 10,
+                                },
+                                style: {
+                                    fill: '#45c946'
+                                }
+                            }],
+                        }
                     }
+                }],
+                grid: {
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    // M886.6 329.4c-6.2-6.2-14.4-9.4-22.6-9.4H160c-8.2 0-16.4 3.1-22.6 9.4-6.2 6.2-9.4 14.4-9.4 22.6s3.1 16.4 9.4 22.6l352 352c6.2 6.2 14.4 9.4 22.6 9.4s16.4-3.1 22.6-9.4l352-352c6.2-6.2 9.4-14.4 9.4-22.6s-3.1-16.4-9.4-22.6z
                 }
-            }],
-            grid: {
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                // M886.6 329.4c-6.2-6.2-14.4-9.4-22.6-9.4H160c-8.2 0-16.4 3.1-22.6 9.4-6.2 6.2-9.4 14.4-9.4 22.6s3.1 16.4 9.4 22.6l352 352c6.2 6.2 14.4 9.4 22.6 9.4s16.4-3.1 22.6-9.4l352-352c6.2-6.2 9.4-14.4 9.4-22.6s-3.1-16.4-9.4-22.6z
-            }
             }
         }
     },
