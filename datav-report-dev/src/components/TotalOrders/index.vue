a8197c005a9008d59e607b4a877173274f81a659
AurorePaladin <aurorepaladin@gmail.com>
2022-01-20 21:49:50 +0800

    用vue-echarts改造-累计订单量

--- datav-report-dev/src/components/TotalOrders/index.vue
+++ datav-report-dev/src/components/TotalOrders/index.vue
@@ -1,7 +1,8 @@
 <template>
     <common-card title="累积订单量" value="2,157,420">
     <template v-slot:wrapper>
-        <div id="total-orders-chart" :style="{ width: '100%',height: '100%'}"></div>
+        <!-- <div id="total-orders-chart" :style="{ width: '100%',height: '100%'}"></div> -->
+        <v-chart :option="getOptions()" />
     </template>
     <template v-slot:footer>
         <span>昨日订单量</span>
@@ -14,40 +15,74 @@
 import commonCardMixin from '../../mixins/commonCardMixin'
 export default {
     mixins: [commonCardMixin],
-    mounted() {
-        const chartDom = document.getElementById('total-orders-chart')
-        const chart = this.$echarts.init(chartDom)
-        chart.setOption({
-            xAxis: {
-                type: 'category',
-                show: false,
-                boundaryGap: false
-            },
-            yAxis: {
-                show: false,
-            },
-            series: [{
-                type: 'line',
-                data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
-                areaStyle: {
-                    color: 'purple',
+    methods: {
+        getOptions: function() {
+            return {
+                xAxis: {
+                    type: 'category',
+                    show: false,
+                    boundaryGap: false
                 },
-                lineStyle: {
-                    width: 0
+                yAxis: {
+                    show: false,
                 },
-                itemStyle: {
-                    opacity: 0
-                },
-                smooth: true
-            }],
-            grid: {
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
+                series: [{
+                    type: 'line',
+                    data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
+                    areaStyle: {
+                        color: 'purple',
+                    },
+                    lineStyle: {
+                        width: 0
+                    },
+                    itemStyle: {
+                        opacity: 0
+                    },
+                    smooth: true
+                }],
+                grid: {
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                }
             }
-        })
-    },
+        }
+    }
+    // mounted() {
+    //     const chartDom = document.getElementById('total-orders-chart')
+    //     const chart = this.$echarts.init(chartDom)
+    //     chart.setOption({
+    //         xAxis: {
+    //             type: 'category',
+    //             show: false,
+    //             boundaryGap: false
+    //         },
+    //         yAxis: {
+    //             show: false,
+    //         },
+    //         series: [{
+    //             type: 'line',
+    //             data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
+    //             areaStyle: {
+    //                 color: 'purple',
+    //             },
+    //             lineStyle: {
+    //                 width: 0
+    //             },
+    //             itemStyle: {
+    //                 opacity: 0
+    //             },
+    //             smooth: true
+    //         }],
+    //         grid: {
+    //             top: 0,
+    //             bottom: 0,
+    //             left: 0,
+    //             right: 0,
+    //         }
+    //     })
+    // },
 }
 </script>
 
