345d5d043b157c7b01fe2c5e30c5a7ea2e1e98ef
AurorePaladin <aurorepaladin@gmail.com>
2022-01-31 17:43:02 +0800

    Bmap组件显示地图

--- datav-report-dev/src/views/BMap.vue
+++ datav-report-dev/src/views/BMap.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="bmap">
-    <h1>This is an bmap page</h1>
-  </div>
+  <v-chart :option="options" />
 </template>
+
+<script>
+import 'echarts/extension/bmap/bmap'
+  export default {
+    data() {
+      return {
+        options: {}
+      }
+    },
+    mounted() {
+      this.options = {
+        bmap: {
+          key: 'G1LFyjrNGIkns5OfpZnrCGAKxpycPLwb',
+          center: [104.114129, 37.550339],
+          zoom: 5, // 缩放比例
+          roam: false, // 是否允许缩放
+          // mapStyle: {}
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
