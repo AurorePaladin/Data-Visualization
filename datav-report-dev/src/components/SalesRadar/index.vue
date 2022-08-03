8d03dc22a04b3d827b79d21e167315b87b797db7
AurorePaladin <aurorepaladin@gmail.com>
2022-08-03 23:04:58 +0800

    watch方法

---
+++ datav-report-dev/src/components/SalesRadar/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sales-radar">
+    <div class="sales-radar-inner">
+      <vue-echarts :options="options" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SalesRadar',
+    data () {
+      return {
+        options: {}
+      }
+    },
+    mounted () {
+      // 渲染echarts雷达图
+      this.options = {}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sales-radar {
+    position: absolute;
+    top: 3250px;
+    left: 0;
+    z-index: 10;
+    width: 100%;
+    height: 800px;
+    padding: 25px 25px 0;
+    box-sizing: border-box;
+
+    .sales-radar-inner {
+      width: 100%;
+      height: 100%;
+      background: rgba(255, 255, 255, .05);
+    }
+  }
+</style>
--- datav-report-dev/src/views/Test.vue
+++ datav-report-dev/src/views/Test.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="test">
     <h1>test count: {{count}}</h1>
+    <div>count * 2 = {{doubleCount}}</div>
     <button @click="add">add</button>
   </div>
 </template>
 
 <script>
-  import { ref } from 'vue'
+  import { ref, computed, watch } from 'vue'
 
   export default {
     setup () {
@@ -14,8 +15,13 @@
       const add = () => {
         count.value++
       }
+      watch(() => count.value, val => {
+        console.log(`count is ${val}`)
+      })
+      const doubleCount = computed(() => count.value * 2)
       return {
         count,
+        doubleCount,
         add
       }
     }
